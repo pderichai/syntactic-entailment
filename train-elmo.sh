7133885a7846aa1a18d93ae7cb29e2a6528069ae
Deric Pang <dericp@cs.washington.edu>
2018-11-20 22:11:48 -0800

    Training decomposable attention with ELMo now working.

---
+++ train-elmo.sh
@@ -0,0 +1,3 @@
+SERIALIZATION_DIR="/tmp/syntactic_entailment_elmo"
+rm -rf "$SERIALIZATION_DIR"
+allennlp train syntactic_entailment_elmo.jsonnet --serialization-dir "$SERIALIZATION_DIR" --include-package syntactic_entailment
