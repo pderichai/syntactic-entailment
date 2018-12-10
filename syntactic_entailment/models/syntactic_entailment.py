f47607a61cd62cffe131b15f78ffa0aed837a8bd
Deric Pang <dericp@cs.washington.edu>
2018-12-10 03:04:58 -0800

    Removing random cuda call.

--- syntactic_entailment/models/syntactic_entailment.py
+++ syntactic_entailment/models/syntactic_entailment.py
@@ -95,7 +95,6 @@ class SyntacticEntailment(Model):
 
         self._predictor = Predictor.from_path("models/elmo-constituency-parser/model.tgz",
                                               predictor_name="syntactic-entailment-constituency-parser")
-        self._predictor._model.cuda()
 
         initializer(self)
 
