3ef25feb8a0a6ea5009784c926ef6bed4f25aac5
Deric Pang <dericp@cs.washington.edu>
2019-03-20 00:03:35 -0700

    Cleaning up parser namings.

--- syntactic_entailment/models/constituency_parser_v2.py
+++ syntactic_entailment/models/constituency_parser_v2.py
@@ -22,7 +22,7 @@ from allennlp.common.checks import ConfigurationError
 from allennlp.models.constituency_parser import SpanConstituencyParser
 from allennlp.models.constituency_parser import SpanInformation
 
-@Model.register("syntactic_entailment_constituency_parser_v2")
+@Model.register("syntactic-entailment-constituency-parser-v2")
 class SyntacticEntailmentSpanConstituencyParser(SpanConstituencyParser):
     """
     This ``SpanConstituencyParser`` simply encodes a sequence of text
