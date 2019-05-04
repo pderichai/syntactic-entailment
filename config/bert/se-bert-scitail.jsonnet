6582d517cb14db93119222439f1add4403da654b
Deric Pang <dericp@cs.washington.edu>
2019-05-04 14:04:03 -0700

    Starting to implement se-bert.

--- config/bert/se-bert-scitail.jsonnet
+++ config/bert/se-bert-scitail.jsonnet
@@ -1,14 +1,30 @@
 {
   "dataset_reader": {
-    "type": "bert-snli",
+    "type": "se-bert-snli",
     "pretrained_bert_model_file": "bert-base-uncased",
+    "token_indexers": {
+      "se-tokens": {
+        "namespace": "se-tokens",
+        "type": "single_id",
+        "lowercase_tokens": true
+      },
+      "tokens": {
+        "type": "single_id"
+      }
+    },
+    "tokenizer": {
+      //"end_tokens": ["@@NULL@@"],
+      "word_splitter": {
+        "type": "spacy",
+        "pos_tags": true
+      }
+    }
   },
   "train_data_path": "SciTailV1.1/snli_format/scitail_1.0_train.txt",
   "validation_data_path": "SciTailV1.1/snli_format/scitail_1.0_dev.txt",
   "model": {
-    "type": "bert-nli",
+    "type": "se-bert-nli",
     "pretrained_bert_model_file": "bert-base-uncased",
-    "num_labels": 2,
   },
   "iterator": {
     "type": "basic",
---
+++ syntactic_entailment/data/dataset_readers/bert/se_bert_snli.py
@@ -0,0 +1,223 @@
+from typing import Dict
+import json
+import logging
+
+from overrides import overrides
+
+from allennlp.common.file_utils import cached_path
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.fields import Field, TextField, LabelField, MetadataField
+from allennlp.data.instance import Instance
+from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
+from allennlp.data.tokenizers import Tokenizer, WordTokenizer
+
+from pytorch_pretrained_bert import BertTokenizer
+import torch
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, text_a, text_b=None, label=None):
+        """Constructs a InputExample.
+
+        Args:
+            text_a: string. The untokenized text of the first sequence. For single
+            sequence tasks, only this sequence must be specified.
+            text_b: (Optional) string. The untokenized text of the second sequence.
+            Only must be specified for sequence pair tasks.
+            label: (Optional) string. The label of the example. This should be
+            specified for train and dev examples, but not for test examples.
+        """
+        self.text_a = text_a
+        self.text_b = text_b
+        self.label = label
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, input_mask, segment_ids):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+
+
+def _truncate_seq_pair(tokens_a, tokens_b, max_length):
+    """Truncates a sequence pair in place to the maximum length."""
+
+    # This is a simple heuristic which will always truncate the longer sequence
+    # one token at a time. This makes more sense than truncating an equal percent
+    # of tokens from each, since if one sequence is very short then each token
+    # that's truncated likely contains more information than a longer sequence.
+    while True:
+        total_length = len(tokens_a) + len(tokens_b)
+        if total_length <= max_length:
+            break
+        if len(tokens_a) > len(tokens_b):
+            tokens_a.pop()
+        else:
+            tokens_b.pop()
+
+
+def convert_example_to_features(example, max_seq_length, tokenizer):
+    tokens_a = tokenizer.tokenize(example.text_a)
+
+    tokens_b = None
+    if example.text_b:
+        tokens_b = tokenizer.tokenize(example.text_b)
+        # Modifies `tokens_a` and `tokens_b` in place so that the total
+        # length is less than the specified length.
+        # Account for [CLS], [SEP], [SEP] with "- 3"
+        _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
+    else:
+        # Account for [CLS] and [SEP] with "- 2"
+        if len(tokens_a) > max_seq_length - 2:
+            tokens_a = tokens_a[:(max_seq_length - 2)]
+
+    # The convention in BERT is:
+    # (a) For sequence pairs:
+    #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+    #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
+    # (b) For single sequences:
+    #  tokens:   [CLS] the dog is hairy . [SEP]
+    #  type_ids: 0   0   0   0  0     0 0
+    #
+    # Where "type_ids" are used to indicate whether this is the first
+    # sequence or the second sequence. The embedding vectors for `type=0` and
+    # `type=1` were learned during pre-training and are added to the wordpiece
+    # embedding vector (and position vector). This is not *strictly* necessary
+    # since the [SEP] token unambiguously separates the sequences, but it makes
+    # it easier for the model to learn the concept of sequences.
+    #
+    # For classification tasks, the first vector (corresponding to [CLS]) is
+    # used as as the "sentence vector". Note that this only makes sense because
+    # the entire model is fine-tuned.
+    tokens = ["[CLS]"] + tokens_a + ["[SEP]"]
+    segment_ids = [0] * len(tokens)
+
+    if tokens_b:
+        tokens += tokens_b + ["[SEP]"]
+        segment_ids += [1] * (len(tokens_b) + 1)
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    padding = [0] * (max_seq_length - len(input_ids))
+    input_ids += padding
+    input_mask += padding
+    segment_ids += padding
+
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+
+
+    return InputFeatures(input_ids=input_ids,
+                         input_mask=input_mask,
+                         segment_ids=segment_ids)
+
+
+@DatasetReader.register("se-bert-snli")
+class SyntacticEntailmentSnliReader(DatasetReader):
+    """
+    Reads a file from the Stanford Natural Language Inference (SNLI) dataset.  This data is
+    formatted as jsonl, one json-formatted instance per line.  The keys in the data are
+    "gold_label", "sentence1", and "sentence2".  We convert these keys into fields named "label",
+    "premise" and "hypothesis", along with a metadata field containing the tokenized strings of the
+    premise and hypothesis.
+
+    Parameters
+    ----------
+    tokenizer : ``Tokenizer``, optional (default=``WordTokenizer()``)
+        We use this ``Tokenizer`` for both the premise and the hypothesis.  See :class:`Tokenizer`.
+    token_indexers : ``Dict[str, TokenIndexer]``, optional (default=``{"tokens": SingleIdTokenIndexer()}``)
+        We similarly use this for both the premise and the hypothesis.  See :class:`TokenIndexer`.
+    """
+
+    def __init__(self,
+                 pretrained_bert_model_file: str,
+                 max_sequence_length: int = 128,
+                 #tokenizer: Tokenizer = None,
+                 #token_indexers: Dict[str, TokenIndexer] = None,
+                 lazy: bool = False) -> None:
+        super().__init__(lazy)
+        self._bert_tokenizer = BertTokenizer.from_pretrained(pretrained_bert_model_file)
+        self._tokenizer = tokenizer
+        self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
+        self._max_sequence_length = max_sequence_length
+
+    @overrides
+    def _read(self, file_path: str):
+        # if `file_path` is a URL, redirect to the cache
+        file_path = cached_path(file_path)
+
+        with open(file_path, 'r') as snli_file:
+            logger.info("Reading SNLI instances from jsonl dataset at: %s", file_path)
+            for line in snli_file:
+                example = json.loads(line)
+
+                label = example["gold_label"]
+                if label == '-':
+                    # These were cases where the annotators disagreed; we'll just skip them.  It's
+                    # like 800 out of 500k examples in the training data.
+                    continue
+
+                premise = example["sentence1"]
+                hypothesis = example["sentence2"]
+
+                yield self.text_to_instance(premise, hypothesis, label)
+
+    @overrides
+    def text_to_instance(self,  # type: ignore
+                         premise: str,
+                         hypothesis: str,
+                         label: str = None) -> Instance:
+        # pylint: disable=arguments-differ
+        fields: Dict[str, Field] = {}
+
+        # syntactic entailment processing
+        premise_tokens = self._tokenizer.tokenize(premise)
+        premise_tags = [x.tag_ for x in premise_tokens]
+        hypothesis_tokens = self._tokenizer.tokenize(hypothesis)
+        hypothesis_tags = [x.tag_ for x in hypothesis_tokens]
+        fields['premise'] = TextField(premise_tokens, self._token_indexers)
+        fields['hypothesis'] = TextField(hypothesis_tokens, self._token_indexers)
+        fields['premise_tags'] = SequenceLabelField(premise_tags,
+                                                    fields['premise'],
+                                                    label_namespace='pos')
+        fields['hypothesis_tags'] = SequenceLabelField(hypothesis_tags,
+                                                       fields['hypothesis'],
+                                                       label_namespace='pos')
+
+        # BERT processing
+        input_example = InputExample(premise, hypothesis)
+        input_features = convert_example_to_features(input_example,
+                                                     self._max_sequence_length,
+                                                     self._bert_tokenizer)
+        input_ids_tensor = torch.tensor(input_features.input_ids,
+                                        dtype=torch.long)
+        input_mask_tensor = torch.tensor(input_features.input_mask,
+                                         dtype=torch.long)
+        segment_ids_tensor = torch.tensor(input_features.segment_ids,
+                                          dtype=torch.long)
+        fields['input_ids'] = MetadataField(input_ids_tensor)
+        fields['token_type_ids'] = MetadataField(segment_ids_tensor)
+        fields['attention_mask'] = MetadataField(input_mask_tensor)
+
+        if label:
+            fields['label'] = LabelField(label)
+
+        #metadata = {"premise_tokens": [x.text for x in premise_tokens],
+        #            "hypothesis_tokens": [x.text for x in hypothesis_tokens],
+        #            "premise_tags": [x.tag_ for x in premise_tokens],
+        #            "hypothesis_tags": [x.tag_ for x in hypothesis_tokens]}
+        #fields["metadata"] = MetadataField(metadata)
+
+        return Instance(fields)
